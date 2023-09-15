f00233e8056e426dcfcf3d6409a33e6f774ce5c4
Yeon-seong <dd300@naver.com>
2023-09-15 21:44:26 +0900

    트위터 클론 : 메뉴 창 디자인 antd Menu 컴포넌트 사용

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -4,15 +4,24 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import Link from 'next/link'; 
+import { Menu } from 'antd';
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <div>
-        <Link href="/">노드버드</Link>
-        <Link href="/profile">프로필</Link>
-        <Link href="/signup">회원가입</Link>
-      </div>
+      <Menu mode="horizontal">
+				<Menu.Item>
+					<Link href="/"><a>노드버드</a></Link>
+				</Menu.Item>
+        
+				<Menu.Item>
+					<Link href="/profile"><a>프로필</a></Link>
+				</Menu.Item>
+
+				<Menu.Item>
+					<Link href="/signup"><a>회원가입</a></Link>
+				</Menu.Item>
+      </Menu>
       { children }
     </div>
   );
