1f27a1f0970059a5c902cde080868209785645b2
Yeon-seong <dd300@naver.com>
2023-09-15 22:21:20 +0900

    트위터 클론 : 프로필 페이지, 회원가입 페이지 문서 제목 변경 next Head 컴포넌트 사용

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -2,11 +2,18 @@
 
 
 import React from 'react';
+import Head from 'next/head';
+
 import AppLayout from '../components/AppLayout';
 
 const Profile = () => {
   return (
-    <AppLayout>내 프로필</AppLayout>
+    <>
+      <Head>
+        <title>내 프로필 | NodeBird</title>
+      </Head>
+      <AppLayout>내 프로필</AppLayout>
+    </>
   );
 }
 
--- front/pages/signup.js
+++ front/pages/signup.js
@@ -2,11 +2,18 @@
 
 
 import React from 'react';
+import Head from 'next/head';
+
 import AppLayout from '../components/AppLayout';
 
 const Signup = () => {
   return (
-    <AppLayout>회원가입 페이지</AppLayout>
+    <>
+      <Head>
+        <title>회원가입 | NodeBird</title>
+      </Head>
+      <AppLayout>회원가입 페이지</AppLayout>
+    </>
   );
 }
 
