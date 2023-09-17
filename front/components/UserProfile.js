08182b0cf1c8a78777c5dcb56825f45f656fb64e
Yeon-seong <dd300@naver.com>
2023-09-17 23:08:39 +0900

    트위터 클론 : 사용자 프로필 제작 antd Card, Avatar 컴포넌트 사용

--- front/components/UserProfile.js
+++ front/components/UserProfile.js
@@ -3,11 +3,27 @@
 
 // 외부 컴포넌트 불러오기
 import React from 'react';
+import { Card, Avatar } from 'antd';
 
 
 
 // 사용자 프로필 컴포넌트(사용자 정의 태그)
-const UserProfile = () => {}
+const UserProfile = () => {
+  return (
+    <Card
+      actions={[
+        <div key="twit">트윗<br />0</div>,
+        <div key="followings">팔로잉<br />0</div>,
+        <div key="followers">팔로워<br />0</div>,
+      ]}
+    >
+      <Card.Meta
+        avatar={<Avatar>YS</Avatar>}
+        title="YeonSeong"
+      />
+    </Card>
+  );
+};
 
 
 
