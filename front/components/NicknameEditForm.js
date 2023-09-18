7a535062158165984d451cb8bd9af471d052d64c
Yeon-seong <dd300@naver.com>
2023-09-18 20:10:05 +0900

    트위터 클론 : 닉네임 수정 폼, 팔로우 목록 생성

---
+++ front/components/NicknameEditForm.js
@@ -0,0 +1,15 @@
+/* -------------------- 트위터 닉네임 수정 폼 -------------------- */
+
+
+// 외부 컴포넌트 불러오기
+import React from 'react';
+
+
+
+// 닉네임 에디터 컴포넌트(사용자 정의 태그)
+const NicknameEditForm = () => {};
+
+
+
+// 닉네임 에디터 컴포넌트 내보내기
+export default NicknameEditForm;
