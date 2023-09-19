b6f3c0d3aae4b2b5bd899459f580d7cc8a66fe94
Yeon-seong <dd300@naver.com>
2023-09-19 23:56:01 +0900

    트위터 클론 : 커스텀 useInput Hooks 에러 해결 화살표 함수 사용으로 발생한 에러를 해결

--- front/hooks/useInput.js
+++ front/hooks/useInput.js
@@ -7,12 +7,14 @@ import { useState, useCallback } from 'react';
 
 
 // 커스텀 Hooks로 중복 코드 제거
-export default (initialValue = null) => {
+export default function useInput(initialValue = null) {
   /* 컴포넌트 상태 저장 : 리액트 Hooks useState 사용 */
   const [value, setValue] = useState(initialValue);
   /* 컴포넌트의 속성(props)으로 넘겨주는 값들은 useCallback 사용 */
   const handler = useCallback((e) => {
     setValue(e.target.value);
   }, []);
+  /* value에 들어가는 값 : id, nickname, password */
+  /* handler에 들어가는 값 : onChangeId, onChangeNickname, onChangePassword*/
   return [value, handler];
 };
