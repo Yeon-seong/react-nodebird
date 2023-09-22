1bf9639e4584b5155ffe3a94feee49119c562ed8
Yeon-seong <dd300@naver.com>
2023-09-22 22:38:27 +0900

    트위터 클론 : 분리한 중앙 데이터 저장소 데이터 합치기 redux combineReducers 함수를 사용해 쪼갠 리듀서 합치기

--- front/reducers/index.js
+++ front/reducers/index.js
@@ -1,34 +1,33 @@
-/* -------------------- 리덕스 구현 -------------------- */
+/* -------------------- 메인 데이터 리듀서 -------------------- */
 
 
 // 불러오기
 import { HYDRATE } from 'next-redux-wrapper';
+import { combineReducers } from 'redux';
 
+// 리듀서 불러오기
+import user from './user';
+import post from './post';
 
-
-// 중앙 데이터 저장소(기본 state)
-const initialState = {
-  /* 사용자 데이터 */
-  user: {
-
-  },
-  /* 포스트 데이터 */
-  post: {
-    
-  }
-};
 
 
 // 리듀서(reducer) : (이전 상태, 액션) => 다음 상태를 만들어내는 함수
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case HYDRATE:
-      console.log('HYDRATE', action);
-      return { ...state, ...payload };
-    default:
-      return state;
-  }
-};
+const rootReducer = combineReducers ({
+  /* 리덕스 SSR을 위해 index 리듀서 추가 */
+  index: (state = {}, action) => {
+    switch (action.type) {
+      case HYDRATE:
+        console.log('HYDRATE', action);
+        return { ...state, ...payload };
+      default:
+        return state;
+    }
+  },
+  /* user 안의 사용자 initialState */
+  user,
+  /* post) 안의 포스트 initialState */
+  post,
+});
 
 
 
