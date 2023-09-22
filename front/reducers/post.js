394114b3d9b1149c188a3eac5604498d960d017f
Yeon-seong <dd300@naver.com>
2023-09-23 03:39:28 +0900

    트위터 클론 : 메인 포스트 더미 데이터 제작 User, Image, Comments 첫 글자를 대문자로 표기

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -4,7 +4,42 @@
 
 // 중앙 데이터 저장소(기본 state)
 export const initialState = {
-  mainPosts: [],
+  /* ---------- 메인 포스트 더미 데이터 ---------- */
+  mainPosts: [{
+    id: 1,
+    User: {
+      id: 1,
+      nickname: 'yeonseong',
+    },
+    content: '첫 번째 게시글 #해시태그 #익스프레스',
+    Image: [{
+      // 이미지 주소1,
+      src: 'https://ibb.co/HHdLL6X',
+    }, {
+      // 이미지 주소2,
+      src: 'https://ibb.co/8mFP9Y8',
+    }, {
+      // 이미지 주소3,
+      src: 'https://ibb.co/sQxgmbX',
+    }],
+    Comments: [{
+      /* ----- 사용자1 : 닉네임, 답글 ----- */
+      User: {
+        nickname: 'ailette',
+      },
+      content: '안녕하세요!',
+    }, {
+      /* ----- 사용자2 : 닉네임, 답글 ----- */
+      User: {
+        nickname: 'dahlia',
+      },
+      content: '만나서 반가워요~',
+    }]
+  }],
+  /* ---------- 이미지 업로드 시 경로 저장 ---------- */
+  imagePaths: [],
+  /* ---------- 포스트 추가 완료 시 true ---------- */
+  postAdded: false,
 }
 
 
