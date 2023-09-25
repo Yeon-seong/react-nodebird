7d70e47c139f106dfc4988307f33bd73747b75ac
Yeon-seong <dd300@naver.com>
2023-09-25 21:48:03 +0900

    트위터 클론 : 포스트 폼 제작 포스트 폼 컴포넌트 나누기 및 구성

--- front/components/PostForm.js
+++ front/components/PostForm.js
@@ -2,14 +2,63 @@
 
 
 // 외부 컴포넌트 불러오기
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { Form, Input, Button } from 'antd';
+import { useSelector } from 'react-redux';
 
 
 
 // 포스트 폼 컴포넌트(사용자 정의 태그)
 const PostForm = () => {
+  const { imagePaths } = useSelector((state) => state.post);
+  const [text, onChangeText] = useState('');
+  const onSubmit = useCallback(() => {}, []);
+  
   return (
-    <div>PostForm</div>
+    <Form
+      style={{ margin: '10px 0px 20px' }}
+      encType="multipart/form-data"
+      onFinish={onSubmit}
+    >
+      <Input.TextArea
+        id="post-form"
+        value={text}
+        onChange={onChangeText}
+        maxLength={140}
+        placeholder="어떤 신기한 일이 있었나요?"
+      />
+      <div>
+        {/* ---------- 파일 업로드 인풋 ---------- */}
+        <input
+          id="file-upload"
+          type="file"
+          multiple hidden
+        />
+        {/* ---------- 이미지 업로드 버튼 ---------- */}
+        <Button>
+          이미지 업로드
+        </Button>
+        {/* ---------- 포스트 작성 버튼 ---------- */}
+        <Button
+          type="primary"
+          style={{ float: 'right' }}
+          htmlType="submit"
+        >
+          트윗하기
+        </Button>
+      </div>
+      <div>
+        {/* ---------- 이미지 업로드 시 미리보기 ---------- */}
+        {imagePaths.map((v) => {
+          <div key={v} style={{ display: 'inline-block' }}>
+            <img src={v} style={{ width: '200px' }} alt={v} />
+            <div>
+              <Button>제거</Button>
+            </div>
+          </div>
+        })}
+      </div>
+    </Form>
   );
 };
 
