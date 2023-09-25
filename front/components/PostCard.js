e9fa2109228832f1f9b97c24b676772e7603d239
Yeon-seong <dd300@naver.com>
2023-09-25 20:53:07 +0900

    트위터 클론 : 포스트 폼, 포스트 카드 생성

---
+++ front/components/PostCard.js
@@ -0,0 +1,19 @@
+/* -------------------- 트위터 포스트 카드 -------------------- */
+
+
+// 외부 컴포넌트 불러오기
+import React from 'react';
+
+
+
+// 포스트 카드 컴포넌트(사용자 정의 태그)
+const PostCard = () => {
+  return (
+    <div>PostCard</div>
+  );
+};
+
+
+
+// 포스트 카드 컴포넌트 내보내기
+export default PostCard;
---
+++ front/components/PostForm.js
@@ -0,0 +1,19 @@
+/* -------------------- 트위터 포스트 폼 -------------------- */
+
+
+// 외부 컴포넌트 불러오기
+import React from 'react';
+
+
+
+// 포스트 폼 컴포넌트(사용자 정의 태그)
+const PostForm = () => {
+  return (
+    <div>PostForm</div>
+  );
+};
+
+
+
+// 포스트 폼 컴포넌트 내보내기
+export default PostForm;
