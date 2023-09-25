d2d954a98268a68ad7aad93242ead4e5b3c4aeb7
Yeon-seong <dd300@naver.com>
2023-09-25 21:40:09 +0900

    트위터 클론 : 로그인했을 때만 포스트 폼 보이기 홈 페이지 컴포넌트 나누기

--- front/pages/index.js
+++ front/pages/index.js
@@ -3,16 +3,27 @@
 
 // 외부 컴포넌트 불러오기
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 // 내부 컴포넌트 불러오기
 import AppLayout from '../components/AppLayout';
+import PostForm from '../components/PostForm';
+import PostCard from '../components/PostCard';
 
 
 
 // 홈 컴포넌트(사용자 정의 태그)
 const Home = () => {
+  const { isLoggedIn } = useSelector((state) => state.user);
+  const { mainPosts } = useSelector((state) => state.post);
+
   return (
-    <AppLayout>홈페이지</AppLayout>
+    <AppLayout>
+      {/* ----- 포스트 폼 ----- */}
+      {isLoggedIn && <PostForm />}
+      {/* ----- 포스트 카드 ----- */}
+      {mainPosts.map((post) => <PostCard key={post.id} post={post} />)}
+    </AppLayout>
   );
 };
 
