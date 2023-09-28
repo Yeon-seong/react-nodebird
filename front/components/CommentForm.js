aef38ac0e8b8a7f903303fda0b67234bf202f0ce
Yeon-seong <dd300@naver.com>
2023-09-28 22:56:27 +0900

    트위터 클론 : 코멘트 폼 생성

---
+++ front/components/CommentForm.js
@@ -0,0 +1,15 @@
+/* -------------------- 트위터 답글 폼 -------------------- */
+
+
+// 외부 컴포넌트 불러오기
+import React from 'react';
+
+
+
+// 코멘트 폼 컴포넌트(사용자 정의 태그)
+const CommentForm = ({ post }) => {};
+
+
+
+// 코멘트 폼 컴포넌트 내보내기
+export default CommentForm;
