ce0967a8b1d03b69bf59701c79e5b4a2c39da405
Yeon-seong <dd300@naver.com>
2023-10-04 23:59:01 +0900

    트위터 클론 : 슬랙 컴포넌트 구성 슬라이드 이미지에 관한 속성 설정

--- front/components/ImagesZoom/index.js
+++ front/components/ImagesZoom/index.js
@@ -1,14 +1,46 @@
 /* -------------------- 트위터 이미지 확대 화면 -------------------- */
 
 
-import React from 'react';
+// 외부 컴포넌트 불러오기
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Slick from 'react-slick';
 
 
 
 // 이미지 줌 컴포넌트(사용자 정의 태그)
 const ImagesZoom = ({ images, onClose }) => {
-  // 
+  /* 현재 슬라이드 상태 저장 */
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  return (
+    <div>
+      {/* ---------- 상세 이미지 헤더 ---------- */}
+      <header>
+        <h1>상세 이미지</h1>
+        <button onClick={onClose}>X</button>
+      </header>
+
+      {/* ---------- 슬랙 컴포넌트 ---------- */}
+      <div>
+        <Slick
+          /* 첫 번째 이미지를 0번째로 하기 */
+          initialSlide={0}
+          /* 현재 슬라이드에서 이미지를 넘기면 다음 이미지 나오기 */
+          afterChange={(slide) => setCurrentSlide}
+          /* 무한 반복 : 마지막 이미지를 넘기면 첫 번째 이미지 나오기 */
+          infinite
+          /* 슬라이드 화살표 지우기 */
+          arrows={false}
+          /* 한 번에 하나의 이미지만 보이기 */
+          slidesToShow={1}
+          /* 한 번에 이미지 하나씩만 넘기기 */
+          slidesToScroll={1}
+        />
+      </div>
+
+    </div>
+  );
 };
 
 
