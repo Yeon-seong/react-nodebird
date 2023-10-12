ecfee948cdb128216e2dbb5b2103450bc2c4faa3
Yeon-seong <dd300@naver.com>
2023-10-12 23:58:41 +0900

    트위터 클론 : Saga 사용 환경 세팅

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -5,21 +5,26 @@
 import { createWrapper } from 'next-redux-wrapper';
 import { createStore, compose, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import creatSagaMiddleware from 'redux-saga';
 
-// rootReducer 불러오기
-import reducer from '../reducers';
+// 리듀서, 루트 사가 불러오기
+import reducer from '../reducers';  // rootReducer
+import rootSaga from '../sagas';    // rootSaga
 
 
 
 // 액션 객체 디스패치(dispatch)
 const configureStore = () => {
-  const middlewares = [];
+  const sagaMiddleware = creatSagaMiddleware();
+  const middlewares = [sagaMiddleware];
   const enhancer = process.env.NODE_ENV === 'production'
   /* ----- 배포용 미들웨어 : 데브툴 연결X ----- */
   ? compose(applyMiddleware(...middlewares))
   /* ----- 개발용 미들웨어 : 데브툴 연결O ----- */
   : composeWithDevTools(applyMiddleware(...middlewares))
   const store = createStore(reducer, enhancer);
+  /* ----- 리덕스 사가(redux-saga) 설정 ----- */
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 
