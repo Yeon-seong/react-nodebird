b6597b069e5c895833613d5f1a340c142d120eb6
Yeon-seong <dd300@naver.com>
2023-10-14 00:00:09 +0900

    트위터 클론 : saga 이펙트로 액션 제작 연습 functon* Generator 함수 사용

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -1,4 +1,33 @@
+// Saga 이펙트 불러오기
+import { all, fork, call, take, put } from 'redux-saga/effects'
+
+// Axios 라이브러리 불러오기
 import axios from 'axios';
+
+
+
+// 로그인 액션
+function* watchLogin() {
+  yield take('LOG_IN_REQUEST', logIn)
+}
+
+// 로그아웃 액션
+function* watchLogOut() {
+  yield take('LOG_OUT_REQUEST')
+}
+
+// 포스트 추가 액션
+function* watchAddPost() {
+  yield take('ADD_POST_REQUEST')
+}
+
+
+// 액션 등록
 export default functon* rootSaga() {
-  // 비동기 액션 삽입
+  /* --- all 배열 안의 코드 동시 실행 --- */
+  yield all([
+    fork(watchLogin),
+    fork(watchLogOut),
+    fork(watchAddPost),
+  ]);
 }
