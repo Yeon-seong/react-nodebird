0b4d48d5e6715331358ea063fb9600bd76db826a
Yeon-seong <dd300@naver.com>
2023-10-17 03:17:15 +0900

    트위터 클론 : saga takeLatest 오타로 인한 에러 해결 Attempted import error

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -2,28 +2,28 @@
 
 
 // Saga 이펙트 불러오기
-import { all, fork, call, takeLates, put, delay } from 'redux-saga/effects';
+import { all, fork, call, takeLatest, put, delay } from 'redux-saga/effects';
 
 // Axios 라이브러리 불러오기
-import axios from 'axios';
+// import axios from 'axios';
 
 
 
 // addPost 실행 시 서버에 addPostAPI 요청
-function addPostAPI(data) {
-  return axios.post('/api/post', data)
-}
+// function addPostAPI(data) {
+//   return axios.post('/api/post', data)
+// }
 
 
 // ADD_POST_REQUEST 액션이 실행되면 addPost 함수 실행
 function* addPost(action) {
   try {
     // const result = yield call(addPostAPI, action.data);
-    yield delay(1000);
     /* ----- 요청 성공 시 ADD_POST_SUCCESS 액션 디스패치 ----- */
+    yield delay(1000);
     yield put({
       type: 'ADD_POST_SUCCESS',
-      data: result.data         // 성공 결과
+      data: action.data,         // 성공 결과
     })
   } catch (err) {
     /* ----- 요청 실패 시 ADD_POST_FAILURE 액션 디스패치 ----- */
@@ -37,7 +37,7 @@ function* addPost(action) {
 
 // 포스트 추가 액션
 function* watchAddPost() {
-  yield takeLates('ADD_POST_REQUEST', addPost);
+  yield takeLatest('ADD_POST_REQUEST', addPost);
 }
 
 
--- front/sagas/user.js
+++ front/sagas/user.js
@@ -2,26 +2,27 @@
 
 
 // Saga 이펙트 불러오기
-import { all, fork, call, takeLates, put, delay } from 'redux-saga/effects';
+import { all, fork, call, takeLatest, put, delay } from 'redux-saga/effects';
 
 // Axios 라이브러리 불러오기
-import axios from 'axios';
+// import axios from 'axios';
 
 
 
 // logIn 실행 시 서버에 logInAPI 요청
-function logInAPI(data) {
-  return axios.post('/api/login', data)
-}
+// function logInAPI(data) {
+//   return axios.post('/api/login', data)
+// }
 // LOG_IN_REQUEST 액션이 실행되면 logIn 함수 실행
 function* logIn(action) {
   try {
+    console.log('saga logIn');
     // const result = yield call(logInAPI, action.data);
-    yield delay(1000);
     /* ----- 요청 성공 시 LOG_IN_SUCCESS 액션 디스패치 ----- */
+    yield delay(1000);
     yield put({
       type: 'LOG_IN_SUCCESS',
-      data: result.data         // 성공 결과
+      data: action.data         // 성공 결과
     })
   } catch (err) {
     /* ----- 요청 실패 시 LOG_IN_FAILURE 액션 디스패치 ----- */
@@ -34,18 +35,17 @@ function* logIn(action) {
 
 
 // logOut 실행 시 서버에 logOutAPI 요청
-function logOutAPI() {
-  return axios.post('/api/logout')
-}
+// function logOutAPI() {
+//   return axios.post('/api/logout')
+// }
 // LOG_OUT_REQUEST 액션이 실행되면 logOut 함수 실행
 function* logOut() {
   try {
     // const result = yield call(logOutAPI);
-    yield delay(1000);
     /* ----- 요청 성공 시 LOG_OUT_SUCCESS 액션 디스패치 ----- */
+    yield delay(1000);
     yield put({
       type: 'LOG_OUT_SUCCESS',
-      data: result.data         // 성공 결과
     })
   } catch (err) {
     /* ----- 요청 실패 시 LOG_OUT_FAILURE 액션 디스패치 ----- */
@@ -59,13 +59,13 @@ function* logOut() {
 
 // 로그인 액션
 function* watchLogin() {
-  yield takeLates('LOG_IN_REQUEST', logIn);
+  yield takeLatest('LOG_IN_REQUEST', logIn);
 }
 
 
 // 로그아웃 액션
 function* watchLogOut() {
-  yield takeLates('LOG_OUT_REQUEST', logOut);
+  yield takeLatest('LOG_OUT_REQUEST', logOut);
 }
 
 
