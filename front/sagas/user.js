bba589bf225c837cae4542175cc2eb99a0ffc5a1
Yeon-seong <dd300@naver.com>
2023-10-18 07:58:24 +0900

    트위터 클론 : 회원가입 saga 액션 추가 회원가입 요청, 성공, 실패 saga 액션 추가

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -11,6 +11,7 @@ import axios from 'axios';
 import {
   LOG_IN_REQUEST, LOG_IN_SUCCESS, LOG_IN_FAILURE,
   LOG_OUT_REQUEST, LOG_OUT_SUCCESS, LOG_OUT_FAILURE,
+  SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE
 } from '../reducers/user';
 
 
@@ -33,7 +34,7 @@ function* logIn(action) {
     /* ----- 요청 실패 시 LOG_IN_FAILURE 액션 디스패치 ----- */
     yield put({
       type: LOG_IN_FAILURE,
-      error: err.response.data,  // 실패 결과
+      error: err.response.data, // 실패 결과
     });
   }
 }
@@ -50,13 +51,36 @@ function* logOut() {
     /* ----- 요청 성공 시 LOG_OUT_SUCCESS 액션 디스패치 ----- */
     yield delay(1000);
     yield put({
-      type: LOG_OUT_SUCCESS,
+      type: LOG_OUT_SUCCESS,    // 성공 결과
     })
   } catch (err) {
     /* ----- 요청 실패 시 LOG_OUT_FAILURE 액션 디스패치 ----- */
     yield put({
       type: LOG_OUT_FAILURE,
-      error: err.response.data,  // 실패 결과
+      error: err.response.data, // 실패 결과
+    });
+  }
+}
+
+
+// signUp 실행 시 서버에 signUpAPI 요청
+function signUpAPI() {
+  return axios.post('/api/signup')
+}
+// SIGN_UP_REQUEST 액션이 실행되면 signUp 함수 실행
+function* signUp() {
+  try {
+    // const result = yield call(signUpAPI);
+    /* ----- 요청 성공 시 SIGN_UP_SUCCESS 액션 디스패치 ----- */
+    yield delay(1000);
+    yield put({
+      type: SIGN_UP_SUCCESS,    // 성공 결과
+    })
+  } catch (err) {
+    /* ----- 요청 실패 시 SIGN_UP_FAILURE 액션 디스패치 ----- */
+    yield put({
+      type: SIGN_UP_FAILURE,
+      error: err.response.data, // 실패 결과
     });
   }
 }
@@ -74,11 +98,18 @@ function* watchLogOut() {
 }
 
 
+// 회원가입 액션
+function* watchSignUp() {
+  yield takeLatest(SIGN_UP_REQUEST, signUp);
+}
+
+
 // 사용자 Saga 액션 등록
 export default function* userSaga() {
-  /* --- all 배열 안의 코드 동시 실행 --- */
+  /* ----- all 배열 안의 코드 동시 실행 ----- */
   yield all([
     fork(watchLogin),
     fork(watchLogOut),
+    fork(watchSignUp),
   ]);
 }
