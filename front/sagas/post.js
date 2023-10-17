0c386b9be89da8a9f4c9b082b4118a121d9036f1
Yeon-seong <dd300@naver.com>
2023-10-18 08:10:29 +0900

    트위터 클론 : 포스트 추가 saga 액션명 수정

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -5,16 +5,14 @@
 import { all, fork, call, takeLatest, put, delay } from 'redux-saga/effects';
 
 // Axios 라이브러리 불러오기
-// import axios from 'axios';
+import axios from 'axios';
 
 
 
 // addPost 실행 시 서버에 addPostAPI 요청
-// function addPostAPI(data) {
-//   return axios.post('/api/post', data)
-// }
-
-
+function addPostAPI(data) {
+  return axios.post('/api/post', data)
+}
 // ADD_POST_REQUEST 액션이 실행되면 addPost 함수 실행
 function* addPost(action) {
   try {
@@ -22,14 +20,14 @@ function* addPost(action) {
     /* ----- 요청 성공 시 ADD_POST_SUCCESS 액션 디스패치 ----- */
     yield delay(1000);
     yield put({
-      type: 'ADD_POST_SUCCESS',
+      type: ADD_POST_SUCCESS,
       data: action.data,         // 성공 결과
     })
   } catch (err) {
     /* ----- 요청 실패 시 ADD_POST_FAILURE 액션 디스패치 ----- */
     yield put({
-      type: 'ADD_POST_FAILURE',
-      data: err.response.data,  // 실패 결과
+      type: ADD_POST_FAILURE,
+      error: err.response.data,  // 실패 결과
     });
   }
 }
@@ -37,7 +35,7 @@ function* addPost(action) {
 
 // 포스트 추가 액션
 function* watchAddPost() {
-  yield takeLatest('ADD_POST_REQUEST', addPost);
+  yield takeLatest(ADD_POST_REQUEST, addPost);
 }
 
 
