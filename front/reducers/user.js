d67dccebf2ab876cbe46e949aa949dde228a60b5
Yeon-seong <dd300@naver.com>
2023-10-17 23:59:27 +0900

    트위터 클론 : 문자열 액션명을 변수로 수정 로그인, 로그아웃, 회원가입, 팔로우, 언팔로우

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -13,10 +13,37 @@ export const initialState = {
 }
 
 
+// 로그인 : 요청, 성공, 실패
+export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
+export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
+
+// 로그아웃 : 요청, 성공, 실패
+export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST';
+export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
+export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE';
+
+// 회원가입 : 요청, 성공, 실패
+export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
+export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
+
+// 팔로우 : 요청, 성공, 실패
+export const FOLLOW_REQUEST = 'FOLLOW_REQUEST';
+export const FOLLOW_SUCCESS = 'FOLLOW_SUCCESS';
+export const FOLLOW_FAILURE = 'FOLLOW_FAILURE';
+
+// 언팔로우 : 요청, 성공, 실패
+export const UNFOLLOW_REQUEST = 'UNFOLLOW_REQUEST';
+export const UNFOLLOW_SUCCESS = 'UNFOLLOW_SUCCESS';
+export const UNFOLLOW_FAILURE = 'UNFOLLOW_FAILURE';
+
+
+
 // 로그인 요청 액션 생성함수(action creator)
 export const loginRequestAction = (data) => {
   return {
-    type: 'LOG_IN_REQUEST',
+    type: LOG_IN_REQUEST,
     data,
   }
 }
@@ -25,7 +52,7 @@ export const loginRequestAction = (data) => {
 // 로그아웃 액션 생성함수(action creator)
 export const logoutRequestAction = () => {
   return {
-    type: 'LOG_OUT_REQUEST',
+    type: LOG_OUT_REQUEST,
   }
 }
 
@@ -34,13 +61,13 @@ export const logoutRequestAction = () => {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     /* ----- 로그인 요청 리듀서 ----- */
-    case 'LOG_IN_REQUEST':
+    case LOG_IN_REQUEST:
       return {
         ...state,
         isLoggingIn: true,
       };
     /* ----- 로그인 성공 리듀서 ----- */
-    case 'LOG_IN_SUCCESS':
+    case LOG_IN_SUCCESS:
       return {
         ...state,
         isLoggingIn: false,
@@ -49,20 +76,20 @@ const reducer = (state = initialState, action) => {
         me: { ...action.data, nickname: 'yeonseong' },
       };
     /* ----- 로그인 실패 리듀서 ----- */
-    case 'LOG_IN_FAILURE':
+    case LOG_IN_FAILURE:
       return {
         ...state,
         isLoggingIn: false,
         isLoggedIn: false,
       };
     /* ----- 로그아웃 요청 리듀서 ----- */
-    case 'LOG_OUT_REQUEST':
+    case LOG_OUT_REQUEST:
       return {
         ...state,
         isLoggingOut: true,
       };
     /* ----- 로그아웃 성공 리듀서 ----- */
-    case 'LOG_OUT_SUCCESS':
+    case LOG_OUT_SUCCESS:
       return {
         ...state,
         isLoggingOut: false,
@@ -70,7 +97,7 @@ const reducer = (state = initialState, action) => {
         me: null,
       };
     /* ----- 로그아웃 실패 리듀서 ----- */
-    case 'LOG_OUT_FAILURE':
+    case LOG_OUT_FAILURE:
       return {
         ...state,
         isLoggingOut: false,
