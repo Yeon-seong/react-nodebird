698c5b65c003805dadaf4cb20d370ffbdf6376d2
Yeon-seong <dd300@naver.com>
2023-10-18 08:31:09 +0900

    트위터 클론 : 답글 추가 액션 제작 답글 추가 요청, 성공, 실패 액션 추가

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -49,7 +49,17 @@ export const ADD_POST_REQUEST = 'ADD_POST_REQUEST';
 export const ADD_POST_SUCCESS = 'ADD_POST_SUCCESS';
 export const ADD_POST_FAILURE = 'ADD_POST_FAILURE';
 
+// 답글 추가 액션 : 요청, 성공, 실패
+export const ADD_COMMENT_REQUEST = 'ADD_COMMENT_REQUEST';
+export const ADD_COMMENT_SUCCESS = 'ADD_COMMENT_SUCCESS';
+export const ADD_COMMENT_FAILURE = 'ADD_COMMENT_FAILURE';
+
 export const addPost = (data) => ({
+  type: ADD_POST_REQUEST,
+  data,
+});
+
+export const addComment = (data) => ({
   type: ADD_POST_REQUEST,
   data,
 });
