a8e359059dab9f00615441cd03daf433ba1f0ea8
Yeon-seong <dd300@naver.com>
2023-10-18 09:15:40 +0900

    트위터 클론 : 답글 추가 saga 액션 제작 답글 추가, 성공, 실패 saga 액션 추가

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -7,6 +7,12 @@ import { all, fork, call, takeLatest, put, delay } from 'redux-saga/effects';
 // Axios 라이브러리 불러오기
 import axios from 'axios';
 
+// reducer 포스트 추가, 답글 추가 액션 불러오기
+import {
+  ADD_POST_REQUEST, ADD_POST_SUCCESS, ADD_POST_FAILURE,
+  ADD_COMMENT_REQUEST, ADD_COMMENT_SUCCESS, ADD_COMMENT_FAILURE,
+} from '../reducers/post';
+
 
 
 // addPost 실행 시 서버에 addPostAPI 요청
@@ -33,9 +39,39 @@ function* addPost(action) {
 }
 
 
+// addComment 실행 시 서버에 addCommentAPI 요청
+function addCommentAPI(data) {
+  return axios.post('/api/post/${data.postId}/comment', data)
+}
+// ADD_POST_REQUEST 액션이 실행되면 addComment 함수 실행
+function* addComment(action) {
+  try {
+    // const result = yield call(addCommentAPI, action.data);
+    /* ----- 요청 성공 시 ADD_COMMENT_SUCCESS 액션 디스패치 ----- */
+    yield delay(1000);
+    yield put({
+      type: ADD_COMMENT_SUCCESS,
+      data: action.data,         // 성공 결과
+    })
+  } catch (err) {
+    /* ----- 요청 실패 시 ADD_COMMENT_FAILURE 액션 디스패치 ----- */
+    yield put({
+      type: ADD_COMMENT_FAILURE,
+      error: err.response.data,  // 실패 결과
+    });
+  }
+}
+
+
 // 포스트 추가 액션
 function* watchAddPost() {
   yield takeLatest(ADD_POST_REQUEST, addPost);
+}
+
+
+// 답글 추가 액션
+function* watchAddComment() {
+  yield takeLatest(ADD_COMMENT_REQUEST, addComment);
 }
 
 
@@ -44,5 +80,6 @@ export default function* postSaga() {
   /* --- all 배열 안의 코드 동시 실행 --- */
   yield all([
     fork(watchAddPost),
+    fork(watchAddComment),
   ]);
 }
