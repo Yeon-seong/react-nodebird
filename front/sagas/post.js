8728f80a6f354ca59b778be5becde94294d982d7
Yeon-seong <dd300@naver.com>
2023-11-03 00:21:57 +0900

    트위터 클론 : 포스트 삭제 API 요청, 성공, 실패 결과 디스패치 redux-saga effects put, try-catch문 사용

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -11,14 +11,15 @@ import axios from 'axios';
 // ShortId 라이브러리 불러오기
 import shortId from 'shortid';
 
-// reducer 포스트 추가, 답글 추가 액션 불러오기
+// reducer 포스트 추가, 답글 추가, 포스트 삭제 액션 불러오기
 import {
   ADD_POST_REQUEST, ADD_POST_SUCCESS, ADD_POST_FAILURE,
-  ADD_COMMENT_REQUEST, ADD_COMMENT_SUCCESS, ADD_COMMENT_FAILURE
+  ADD_COMMENT_REQUEST, ADD_COMMENT_SUCCESS, ADD_COMMENT_FAILURE,
+  REMOVE_POST_REQUEST, REMOVE_POST_SUCCESS, REMOVE_POST_FAILURE
 } from '../reducers/post';
 
-// reducer 내가 작성한 포스트 액션 불러오기
-import { ADD_POST_TO_ME } from '../reducers/user';
+// reducer 내가 작성한 포스트, 내 포스트 삭제 액션 불러오기
+import { ADD_POST_TO_ME, REMOVE_POST_OF_ME } from '../reducers/user';
 
 
 
@@ -56,6 +57,36 @@ function* addPost(action) {
 }
 
 
+// removePost 실행 시 서버에 removePostAPI 요청
+function removePostAPI(data) {
+  return axios.delete('/api/post', data)
+}
+// REMOVE_POST_REQUEST 액션이 실행되면 removePost 함수 실행
+function* removePost(action) {
+  /* ----- 요청 성공 시 REMOVE_POST_SUCCESS 액션 디스패치 ----- */
+  try {
+    // const result = yield call(removePostAPI, action.data);
+    yield delay(1000);
+    yield put({
+      type: REMOVE_POST_SUCCESS,
+      data: action.data,        // 성공 결과
+    }); // post 리듀서 조작 부분
+    /* ----- 요청 성공 시 REMOVE_POST_OF_ME 액션 디스패치 ----- */
+    yield put({
+      type: REMOVE_POST_OF_ME,
+      data: action.data,
+    }); // user 리듀서 조작 부분
+  /* ----- 요청 실패 시 REMOVE_POST_FAILURE 액션 디스패치 ----- */
+  } catch (err) {
+    console.error(err);
+    yield put({
+      type: REMOVE_POST_FAILURE,
+      data: err.response.data,  // 실패 결과
+    });
+  }
+}
+
+
 // addComment 실행 시 서버에 addCommentAPI 요청
 function addCommentAPI(data) {
   return axios.post('/api/post/${data.postId}/comment', data)
@@ -85,6 +116,10 @@ function* watchAddPost() {
   yield takeLatest(ADD_POST_REQUEST, addPost);
 }
 
+// 포스트 삭제 액션
+function* watchRemovePost() {
+  yield takeLatest(REMOVE_POST_REQUEST, removePost);
+}
 
 // 답글 추가 액션
 function* watchAddComment() {
@@ -98,6 +133,7 @@ export default function* postSaga() {
   /* --- all 배열 안의 코드 동시 실행 --- */
   yield all([
     fork(watchAddPost),
+    fork(watchRemovePost),
     fork(watchAddComment),
   ]);
 }
