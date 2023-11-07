004fe74af5896931ba1e766d4ce726728cc2718b
Yeon-seong <dd300@naver.com>
2023-11-07 23:37:36 +0900

    트위터 클론 : 게시글 불러오기 saga 제작 게시글 불러오기 API 요청, 성공, 실패 결과 디스패치

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -11,28 +11,58 @@ import axios from 'axios';
 // ShortId 라이브러리 불러오기
 import shortId from 'shortid';
 
-// 게시글 추가, 답글 추가, 게시글 삭제 액션 불러오기
+// 게시글 불러오기, 게시글 추가, 답글 추가, 게시글 삭제 액션 불러오기
 import {
+  LOAD_POSTS_REQUEST, LOAD_POSTS_SUCCESS, LOAD_POSTS_FAILURE,
   ADD_POST_REQUEST, ADD_POST_SUCCESS, ADD_POST_FAILURE,
-  ADD_COMMENT_REQUEST, ADD_COMMENT_SUCCESS, ADD_COMMENT_FAILURE,
-  REMOVE_POST_REQUEST, REMOVE_POST_SUCCESS, REMOVE_POST_FAILURE
+  REMOVE_POST_REQUEST, REMOVE_POST_SUCCESS, REMOVE_POST_FAILURE,
+  ADD_COMMENT_REQUEST, ADD_COMMENT_SUCCESS, ADD_COMMENT_FAILURE
 } from '../reducers/post';
 
 // 내가 작성한 게시글, 내 게시글 삭제 액션 불러오기
 import { ADD_POST_TO_ME, REMOVE_POST_OF_ME } from '../reducers/user';
 
+// 게시글 더미데이터 불러오기
+import { generateDummyPost } from '../reducers/post';
+
+
+
+// loadPosts 실행 시 서버에 loadPostsAPI 요청
+function loadPostsAPI(data) {
+  return axios.get('/api/posts', data);
+}
+// ADD_POST_REQUEST 액션이 실행되면 loadPosts 함수 실행
+function* loadPosts(action) {
+  /* ---------- 요청 성공 시 LOAD_POSTS_SUCCESS 액션 디스패치 ---------- */
+  try {
+    // const result = yield call(loadPostsAPI, action.data);
+    yield delay(1000);              // 가짜 로딩시간
+    yield put({
+      type: LOAD_POSTS_SUCCESS,
+      data: generateDummyPost(10),  // 성공 결과
+      
+    });
+  /* ---------- 요청 실패 시 LOAD_POSTS_FAILURE 액션 디스패치 ---------- */
+  } catch (err) {
+    console.error(err);
+    yield put({
+      type: LOAD_POSTS_FAILURE,
+      data: err.response.data,      // 실패 결과
+    });
+  }
+}
 
 
 // addPost 실행 시 서버에 addPostAPI 요청
 function addPostAPI(data) {
-  return axios.post('/api/post', data)
+  return axios.post('/api/post', data);
 }
 // ADD_POST_REQUEST 액션이 실행되면 addPost 함수 실행
 function* addPost(action) {
   /* ---------- 요청 성공 시 ADD_POST_SUCCESS 액션 디스패치 ---------- */
   try {
     // const result = yield call(addPostAPI, action.data);
-    yield delay(1000);
+    yield delay(1000);          // 가짜 로딩시간
     const id = shortId.generate();
     yield put({
       type: ADD_POST_SUCCESS,
@@ -59,14 +89,14 @@ function* addPost(action) {
 
 // removePost 실행 시 서버에 removePostAPI 요청
 function removePostAPI(data) {
-  return axios.delete('/api/post', data)
+  return axios.delete('/api/post', data);
 }
 // REMOVE_POST_REQUEST 액션이 실행되면 removePost 함수 실행
 function* removePost(action) {
   /* ---------- 요청 성공 시 REMOVE_POST_SUCCESS 액션 디스패치 ---------- */
   try {
     // const result = yield call(removePostAPI, action.data);
-    yield delay(1000);
+    yield delay(1000);          // 가짜 로딩시간
     yield put({
       type: REMOVE_POST_SUCCESS,
       data: action.data,        // 성공 결과
@@ -89,14 +119,14 @@ function* removePost(action) {
 
 // addComment 실행 시 서버에 addCommentAPI 요청
 function addCommentAPI(data) {
-  return axios.post('/api/post/${data.postId}/comment', data)
+  return axios.post('/api/post/${data.postId}/comment', data);
 }
 // ADD_POST_REQUEST 액션이 실행되면 addComment 함수 실행
 function* addComment(action) {
   /* ---------- 요청 성공 시 ADD_COMMENT_SUCCESS 액션 디스패치 ---------- */
   try {
     // const result = yield call(addCommentAPI, action.data);
-    yield delay(1000);
+    yield delay(1000);          // 가짜 로딩시간
     yield put({
       type: ADD_COMMENT_SUCCESS,
       data: action.data,         // 성공 결과
@@ -110,6 +140,11 @@ function* addComment(action) {
   }
 }
 
+
+// 게시글 불러오기 액션
+function* watchLoadPosts() {
+  yield takeLatest(LOAD_POSTS_REQUEST, loadPosts);
+}
 
 // 게시글 추가 액션
 function* watchAddPost() {
@@ -132,6 +167,7 @@ function* watchAddComment() {
 export default function* postSaga() {
   /* all 배열 안의 코드 동시 실행 */
   yield all([
+    fork(watchLoadPosts),
     fork(watchAddPost),
     fork(watchRemovePost),
     fork(watchAddComment),
