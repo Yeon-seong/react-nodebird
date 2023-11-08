cb4b254fc53ca0882846e6410d6a5555c8ec85af
Yeon-seong <dd300@naver.com>
2023-11-08 23:47:52 +0900

    트위터 클론 : 스크롤을 끝까지 내리면 로딩 후 게시글 더 불러오기 새로운 게시글 불러오기 액션 디스패치

--- front/pages/index.js
+++ front/pages/index.js
@@ -18,16 +18,38 @@ import { LOAD_POSTS_REQUEST } from '../reducers/post';
 
 // 홈 컴포넌트(사용자 정의 태그)
 const Home = () => {
-  /* 처음에 화면을 로딩하면 게시글 불러오기 요청 액션 호출 */
   const dispatch = useDispatch();
+  const { me } = useSelector((state) => state.user);
+  const { mainPosts } = useSelector((state) => state.post);
+
+
+  /* 처음에 화면을 로딩하면 게시글 불러오기 요청 액션 호출 */
   useEffect(() => {
     dispatch({
       type: LOAD_POSTS_REQUEST,
     });
   }, []);
+
+
+  // 스크롤을 끝까지 내렸을 때, 다시 로딩해서 게시글 더 불러오기
+  useEffect(() => {
+    function onScroll() {
+      console.log(window.scrollY, document.documentElement.clientHeight, document.documentElement.scrollHeight);
+      /* Y축 스크롤 이동 값 + 현재 보고있는 화면 높이와 스크롤의 전체 높이가 같으면 */
+      if (window.scrollY + document.documentElement.clientHeight === document.documentElement.scrollHeight) {
+        /* 스크롤을 다 내리면 새로운 게시글 불러오기 액션 디스패치 */
+        dispatch({
+          type: LOAD_POSTS_REQUEST,
+        });
+      }
+    }
+    /* 스크롤 이벤트 리스너 */
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
   
-  const { me } = useSelector((state) => state.user);
-  const { mainPosts } = useSelector((state) => state.post);
 
   return (
     <AppLayout>
