988b7efb5bef48acaf20a9a44c5e74056d9d9c44
Yeon-seong <dd300@naver.com>
2023-11-10 23:48:07 +0900

    트위터 클론 : 팔로우 버튼 컴포넌트 생성

---
+++ front/components/FollowButton.js
@@ -0,0 +1,16 @@
+/* -------------------- 트위터 팔로우 버튼 -------------------- */
+
+
+
+// 외부 컴포넌트 불러오기
+import React from 'react';
+
+
+
+// 팔로우 버튼 컴포넌트(사용자 정의 태그)
+const FollowButton = ({ post }) => {};
+
+
+
+// 팔로우 버튼 컴포넌트 내보내기
+export default FollowButton;
