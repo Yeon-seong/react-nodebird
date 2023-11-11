47af343afdd3708414bf60517e46392b0738b368
Yeon-seong <dd300@naver.com>
2023-11-12 05:51:39 +0900

    트위터 클론 : 팔로우, 언팔로우 버튼 사가(saga) 제작

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -10,11 +10,13 @@ import {
 // Axios 라이브러리 불러오기
 import axios from 'axios';
 
-// 사용자 로그인, 로그아웃, 회원가입 액션 불러오기
+// 사용자 로그인, 로그아웃, 회원가입, 팔로우, 언팔로우 액션 불러오기
 import {
   LOG_IN_REQUEST, LOG_IN_SUCCESS, LOG_IN_FAILURE,
   LOG_OUT_REQUEST, LOG_OUT_SUCCESS, LOG_OUT_FAILURE,
-  SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE
+  SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE,
+  FOLLOW_REQUEST, FOLLOW_SUCCESS, FOLLOW_FAILURE,
+  UNFOLLOW_REQUEST, UNFOLLOW_SUCCESS, UNFOLLOW_FAILURE
 } from '../reducers/user';
 
 
@@ -92,6 +94,57 @@ function* signUp() {
 }
 
 
+// follow 실행 시 서버에 followAPI 요청
+function followAPI(data) {
+  return axios.post('/api/follow', data)
+}
+// FOLLOW_REQUEST 액션이 실행되면 follow 함수 실행
+function* follow(action) {
+  /* ---------- 요청 성공 시 FOLLOW_SUCCESS 액션 디스패치 ---------- */
+  try {
+    // const result = yield call(followAPI, action.data);
+    yield delay(1000);
+    yield put({
+      type: FOLLOW_SUCCESS,
+      data: action.data         // 성공 결과
+    })
+  /* ---------- 요청 실패 시 FOLLOW_FAILURE 액션 디스패치 ---------- */
+  } catch (err) {
+    console.error(err);
+    yield put({
+      type: FOLLOW_FAILURE,
+      error: err.response.data, // 실패 결과
+    });
+  }
+}
+
+
+// unfollow 실행 시 서버에 unfollowAPI 요청
+function unfollowAPI(data) {
+  return axios.post('/api/unfollow', data)
+}
+// UNFOLLOW_REQUEST 액션이 실행되면 unfollow 함수 실행
+function* unfollow(action) {
+  /* ---------- 요청 성공 시 UNFOLLOW_SUCCESS 액션 디스패치 ---------- */
+  try {
+    // const result = yield call(unfollowAPI, action.data);
+    yield delay(1000);
+    yield put({
+      type: UNFOLLOW_SUCCESS,
+      data: action.data         // 성공 결과
+    })
+  /* ---------- 요청 실패 시 UNFOLLOW_FAILURE 액션 디스패치 ---------- */
+  } catch (err) {
+    console.error(err);
+    yield put({
+      type: UNFOLLOW_FAILURE,
+      error: err.response.data, // 실패 결과
+    });
+  }
+}
+
+
+
 // 로그인 액션
 function* watchLogin() {
   yield takeLatest(LOG_IN_REQUEST, logIn);
@@ -110,11 +163,25 @@ function* watchSignUp() {
 }
 
 
+// 팔로우 액션
+function* watchFollow() {
+  yield takeLatest(FOLLOW_REQUEST, follow);
+}
+
+
+// 언팔로우 액션
+function* watchUnFollow() {
+  yield takeLatest(UNFOLLOW_REQUEST, unfollow);
+}
+
+
 
 // 사용자 Saga 액션 등록
 export default function* userSaga() {
   /* ---------- all 배열 안의 코드 동시 실행 ---------- */
   yield all([
+    fork(watchFollow),
+    fork(watchUnFollow),
     fork(watchLogin),
     fork(watchLogOut),
     fork(watchSignUp),
