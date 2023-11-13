a7776a4c37a45f26195182bc37334efa122559fd
Yeon-seong <dd300@naver.com>
2023-11-13 23:07:03 +0900

    트위터 클론 : 익스프레스를 사용해 메인 페이지 가져오기

--- back/app.js
+++ back/app.js
@@ -1,20 +1,24 @@
-// http 서버 모듈 사용 : 웹 브라우저 요청 처리
-const http = require('http');
+/* -------------------- 트위터 API -------------------- */
 
 
-// 요청(Req: Request)
-const server = http.createServer((reg, res) => {
-  console.log(reg.url, reg.method);
-  
-  // 응답(Res: Response)
-  res.write('<h1>Hello node1</h1>');
-  res.write('<h2>Hello node2</h2>');
-  res.write('<h3>Hello node3</h3>');
-  res.write('<h4>Hello node4</h4>');
-  res.end('<h5>Hello node5</h5>');
+
+// Express 라이브러리 사용
+const express = require('express');
+
+
+
+// 호출
+const app = express();
+
+
+// 메인 페이지 가져오기
+app.get('/', (req, res) => {
+  res.send('hello express');
 });
 
 
-server.listen(3000, () => {
+
+// http://localhost:3000/ : 3000 포트로 서버 열기
+app.listen(3000, () => {
   console.log('서버 실행 중');
 });
