4dc63b99aad4cce8bd85851f71649a0cac41533f
Yeon-seong <dd300@naver.com>
2023-11-15 02:18:20 +0900

    트위터 클론 : 시퀄라이즈(SQL)로 Node와 MySQL을 연결하기

--- back/models/index.js
+++ back/models/index.js
@@ -1,29 +1,22 @@
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
+// SQL(시퀄라이즈) 불러오기
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
+
+// 개발 시 환경 변수 기본 값 : development
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+
+// config = development 객체 정보
+const config = require('../config/config')[env];
+
+// 데이터베이스 객체
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+// 시퀄라이즈 객체에 연결 정보가 담김
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
