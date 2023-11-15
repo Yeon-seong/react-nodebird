f2f35486b4874c7ddd7b0703aeb8de5817e76acf
Yeon-seong <dd300@naver.com>
2023-11-16 00:29:02 +0900

    트위터 클론 : 답글 모델 정보의 칼럼 조건설정

--- back/models/comment.js
+++ back/models/comment.js
@@ -5,9 +5,15 @@
 module.exports = (sequelize, DataTypes) => {
   // 답글 모델(테이블)
   const Comment = sequelize.define('Comment', { // MySQL에는 comments 테이블 생성
+    
     /* ---------- 답글 모델 정보 ---------- */
     // id: {},    // id가 기본적으로 들어있다.
-    content: {},
+    // 콘텐츠 칼럼
+    content: {
+      type: DataTypes.TEXT,   // 텍스트 글자 수 무제한
+      allowNull: false,       // 답글 콘텐츠 필수
+    },
+
   }, {
     /* ---------- 답글 모델 세팅 ---------- */
     charset: 'utf8mp4',             // MySQL에서 한글, 이모티콘 사용 가능
