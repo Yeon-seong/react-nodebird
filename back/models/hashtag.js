6d8bf608816d534941ce0c2e96c4b4bfec018574
Yeon-seong <dd300@naver.com>
2023-11-16 00:30:26 +0900

    트위터 클론 : 해시태그 모델 정보의 칼럼 조건설정

--- back/models/hashtag.js
+++ back/models/hashtag.js
@@ -5,9 +5,15 @@
 module.exports = (sequelize, DataTypes) => {
   // 해시태그 모델(테이블)
   const Hashtag = sequelize.define('Hashtag', { // MySQL에는 hashtags 테이블 생성
+
     /* ---------- 해시태그 모델 정보 ---------- */
     // id: {},    // id가 기본적으로 들어있다.
-    name: {},
+    // 이름 칼럼
+    name: {
+      type: DataTypes.STRING(30),   // 30글자 이내의 문자열
+      allowNull: false,             // 해시태그 이름 필수
+    },
+
   }, {
     /* ---------- 해시태그 모델 세팅 ---------- */
     charset: 'utf8mp4',             // MySQL에서 한글, 이모티콘 사용 가능
