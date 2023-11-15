10881954431cbb7f3547b2860675ad8f2dbd6648
Yeon-seong <dd300@naver.com>
2023-11-16 00:27:42 +0900

    트위터 클론 : 게시글 모델 정보의 칼럼 조건설정

--- back/models/post.js
+++ back/models/post.js
@@ -5,9 +5,15 @@
 module.exports = (sequelize, DataTypes) => {
   // 게시글 모델(테이블)
   const Post = sequelize.define('Post', { // MySQL에는 posts 테이블 생성
+    
     /* ---------- 게시글 모델 정보 ---------- */
     // id: {},    // id가 기본적으로 들어있다.
-    content: {},
+    // 콘텐츠 칼럼
+    content: {
+      type: DataTypes.TEXT,   // 텍스트 글자 수 무제한
+      allowNull: false,       // 게시글 콘텐츠 필수
+    },
+
   }, {
     /* ---------- 게시글 모델 세팅 ---------- */
     charset: 'utf8mp4',             // MySQL에서 한글, 이모티콘 사용 가능
