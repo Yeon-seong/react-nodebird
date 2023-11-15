f5821264f5821427e753b6fb0ad2cf154572c9c1
Yeon-seong <dd300@naver.com>
2023-11-16 00:32:05 +0900

    트위터 클론 : 이미지 모델 정보의 칼럼 조건설정

--- back/models/image.js
+++ back/models/image.js
@@ -5,9 +5,15 @@
 module.exports = (sequelize, DataTypes) => {
   // 이미지 모델(테이블)
   const Image = sequelize.define('Image', { // MySQL에는 images 테이블 생성
+
     /* ---------- 이미지 모델 정보 ---------- */
     // id: {},    // id가 기본적으로 들어있다.
-    src: {},
+    // 경로 소스 칼럼
+    src: {
+      type: DataTypes.STRING(300),  // 300글자 이내의 문자열
+      allowNull: false,             // 이미지 경로 소스 필수
+    },
+
   }, {
     /* ---------- 이미지 모델 세팅 ---------- */
     charset: 'utf8',              // MySQL에서 한글 사용 가능
