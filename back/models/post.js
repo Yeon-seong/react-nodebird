b7d6b1cfaa25d1d968de4163915cf095574a6d49
Yeon-seong <dd300@naver.com>
2023-11-15 22:40:35 +0900

    트위터 클론 : 게시글 시퀄라이즈 모델 설계 제작

---
+++ back/models/post.js
@@ -0,0 +1,19 @@
+/* -------------------- 트위터 게시글 모델 -------------------- */
+
+
+
+module.exports = (sequelize, DataTypes) => {
+  // 게시글 모델(테이블)
+  const Post = sequelize.define('Post', { // MySQL에는 posts 테이블 생성
+    /* ---------- 게시글 모델 정보 ---------- */
+    // id: {},    // id가 기본적으로 들어있다.
+    content: {},
+  }, {
+    /* ---------- 게시글 모델 세팅 ---------- */
+    charset: 'utf8mp4',             // MySQL에서 한글, 이모티콘 사용 가능
+    collate: 'utf8mp4_general_ci',  // 한글, 이모티콘 저장
+  });
+  // 게시글 모델 관계 설정
+  Post.associate = (db) => {};
+  return Post;
+};
