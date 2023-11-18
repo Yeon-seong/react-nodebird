69ead0fb3a8d2981c140b04452bf9a2aa452a21e
Yeon-seong <dd300@naver.com>
2023-11-18 23:29:52 +0900

    트위터 클론 : 같은 사용자 모델 내의 팔로잉, 팔로워 N:M(다대다) 관계 설정

--- back/models/user.js
+++ back/models/user.js
@@ -46,6 +46,17 @@ module.exports = (sequelize, DataTypes) => {
     // 중간 테이블 이름 : 'Like', 내 게시글에 좋아요를 누른 사용자 별칭 : 'Likerd'
     db.User.belongsToMany(db.Post, { through: 'Like', as: 'Liked' });
 
+    /* (N:M 관계) : 한 명의 사용자(User)도 여러 사용자(User)에게 팔로우(Follow)를 할 수 있다. */
+    // 중간 테이블 이름 : 'Follow', 별칭 : 'Followers', 칼럼의 아이디 : 'FollowingId'
+    db.User.belongsToMany(
+      db.User, { through: 'Follow', as: 'Followers', foreignKey: 'FollowingId' }
+    );
+
+    // 중간 테이블 이름 : 'Follow', 별칭 : 'Followings', 칼럼의 아이디 : 'FollowerId'
+    db.User.belongsToMany(
+      db.User, { through: 'Follow', as: 'Followings', foreignKey: 'FollowerId' }
+    );
+    
   };
   return User;
 };
