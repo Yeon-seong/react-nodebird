d5d0a13e8ce03ccb16e7b196975afa535526c158
Yeon-seong <dd300@naver.com>
2023-11-18 23:59:29 +0900

    트위터 클론 : 사용자, 게시글, 답글, 해시태그, 이미지 모델 불러오기

--- back/models/index.js
+++ back/models/index.js
@@ -22,6 +22,15 @@ const sequelize = new Sequelize(
   config
 );
 
+
+// 모델 불러오기
+db.User = require('./user')(sequelize, Sequelize);
+db.Post = require('./post')(sequelize, Sequelize);
+db.Comment = require('./comment')(sequelize, Sequelize);
+db.Hashtag = require('./hashtag')(sequelize, Sequelize);
+db.Image = require('./image')(sequelize, Sequelize);
+
+
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
