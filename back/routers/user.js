6991f929c40245d3ce65ad735b4f4d87e403655a
Yeon-seong <dd300@naver.com>
2023-11-20 23:50:37 +0900

    트위터 클론 : 회원가입 시 이메일 중복체크 및 에러 처리 사용자 중 이메일이 같은 사용자 유무에 따른 결과 출력

--- back/routers/user.js
+++ back/routers/user.js
@@ -19,8 +19,26 @@ const router = express.Router();
 // 사용자 라우터
 router.post('/', async (req, res, next) => {  // POST /user/
   try {
-    /* 비밀번호 암호화(해시화) */
+
+    /* 프론트에서 보낸 이메일과 같은 이메일을 쓰는 사용자가 있다면 exUser에 저장 */
+    const exUser = await User.findOne({
+      where: {
+        email: req.body.email,
+      }
+    });
+    
+
+    /* 만약 사용자 중에 이메일이 같은 사용자가 있다면? 400번대 에러 출력 */
+    if (exUser) {
+      return res.status(403).send('이미 사용중인 아이디입니다.'); // status 400
+    }
+
+
+    /* 사용자 중에 이메일이 같은 사용자가 없다면 DB에 저장해서 ok를 보낸다. */
+    // 비밀번호 암호화(해시화)
     const hashedPassword = await bcrypt.hash(req.body.password, 12);
+
+    
     /* await : 실제로 데이터가 들어감, create : 테이블 안에 데이터를 넣음 */
     await User.create({
       email: req.body.email,
@@ -28,10 +46,13 @@ router.post('/', async (req, res, next) => {  // POST /user/
       password: hashedPassword, // 암호화 된 비밀번호 저장
     });
     /* User.create() 비동기 함수가 실행되고 난 다음에 실행 */
-    res.send('ok'); 
+    res.status(201).send('ok'); // status 201
+
+
+  /* 에러 캐치 */
   } catch (error) {
     console.error(error); // 콘솔 창을 통한 에러 메시지 출력
-    next(error);	        // express next를 통한 에러 처리 미들웨어로 에러 보내기
+    next(error);  // status 500 : express next를 통한 에러 처리 미들웨어로 에러 보내기
   }
 });
 
