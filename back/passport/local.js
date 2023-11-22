028cc48ed85a0189a4ebed1d653a6bf65529d04b
Yeon-seong <dd300@naver.com>
2023-11-22 15:16:15 +0900

    트위터 클론 : 로그인 시 이메일 검사에 대한 함수 설정 사용자 유무에 따른 서버 에러, 성공, 클라이언트 에러 출력

--- back/passport/local.js
+++ back/passport/local.js
@@ -8,6 +8,9 @@ const passport = require('passport');
 // 로컬 로그인 전략
 const { Strategy: LocalStrategy } = require('passport-local');
 
+// 사용자 모델 불러오기
+const { User } = require('../models');
+
 
 
 module.exports = () => {
@@ -15,7 +18,18 @@ module.exports = () => {
     /* ---------- 객체 : req.body에 대한 설정 ---------- */
     usernameField: 'email',     // req.body.email
     passwordField: 'password',  // req.body.password
-  }, () => {
-    // 로그인 전략
+  }, async (email, password, done) => {
+    /* ---------- 함수 : 로그인 할 때 입력한 이메일에 사용자가 있는지 검사 ---------- */
+    const User = await User.findOne({
+      where: { email }
+    });
+    /* ---------- 만약 사용자가 없다면 ---------- */
+    if (!User) {
+      done(
+        null,                                     // 첫 번째 자리 : 서버 에러
+        false,                                    // 두 번째 자리 : 성공
+        { reason: '존재하지 않는 사용자입니다!' }   // 세 번째 자리 : 클라이언트 에러
+      );
+    }
   }));
 };
