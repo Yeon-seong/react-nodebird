92c7e323fdf432ed332a7c2c055cc9225ea51adb
Yeon-seong <dd300@naver.com>
2023-11-22 14:37:45 +0900

    트위터 클론 : 로컬 로그인 객체 req.body에 대한 설정

--- back/passport/local.js
+++ back/passport/local.js
@@ -11,5 +11,11 @@ const { Strategy: LocalStrategy } = require('passport-local');
 
 
 module.exports = () => {
-  passport.use(new LocalStrategy());
+  passport.use(new LocalStrategy({
+    /* ---------- 객체 : req.body에 대한 설정 ---------- */
+    usernameField: 'email',     // req.body.email
+    passwordField: 'password',  // req.body.password
+  }, () => {
+    // 로그인 전략
+  }));
 };
