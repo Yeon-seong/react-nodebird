bd0fb07ddd73bfe7e246f0e4e9916dc48afc933e
Yeon-seong <dd300@naver.com>
2023-11-22 23:58:05 +0900

    트위터 클론 : 로그인 시 비밀번호 일치 유무에 대한 함수 설정 비밀번호 일치 유무에 대한 서버 에러, 성공, 클라이언트 에러 출력

--- back/passport/local.js
+++ back/passport/local.js
@@ -8,6 +8,9 @@ const passport = require('passport');
 // 로컬 로그인 전략
 const { Strategy: LocalStrategy } = require('passport-local');
 
+// 비밀번호 암호화 라이브러리
+const bcrypt = require('bcrypt');
+
 // 사용자 모델 불러오기
 const { User } = require('../models');
 
@@ -19,17 +22,28 @@ module.exports = () => {
     usernameField: 'email',     // req.body.email
     passwordField: 'password',  // req.body.password
   }, async (email, password, done) => {
-    /* ---------- 함수 : 로그인 할 때 입력한 이메일에 사용자가 있는지 검사 ---------- */
-    const user = await User.findOne({
-      where: { email }
-    });
-    /* ---------- 만약 사용자가 없다면 ---------- */
-    if (!user) {
-      return done(
-        null,                                     // 첫 번째 자리 : 서버 에러
-        false,                                    // 두 번째 자리 : 성공
-        { reason: '존재하지 않는 사용자입니다!' }   // 세 번째 자리 : 클라이언트 에러
-      );
-    };
+    /* ---------- 함수 : 로그인 할 때 입력한 이메일이 있는지 검사 ---------- */
+    try {
+      const user = await User.findOne({
+        where: { email }
+      });
+      /* ---------- 만약 사용자(이메일)가 없다면 클라이언트 실패 ---------- */
+      if (!user) {
+        return done(null, false, { reason: '존재하지 않는 이메일입니다!' });
+      };
+
+      // DB에 저장된 비밀번호와 사용자가 입력한 비밀번호를 await으로 비교
+      const result = await bcrypt.compare(password, user.passport);
+      /* 만약 이메일이 있고, 비밀번호가 일치하면 로그인 성공 */
+      if (result) {
+        return done(null, user);  // 성공 시 사용자 정보를 넘김
+      };
+      /* ---------- 비밀번호가 일치하지 않으면 클라이언트 실패 ---------- */
+      return done(null, false, { reason: '비밀번호가 틀렸습니다.' });
+    } catch (error) {
+      /* ---------- 서버 에러가 난다면 ---------- */
+      console.error(error);
+      return done(error); // 서버 에러가 난 경우
+    }
   }));
 };
