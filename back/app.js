824a803e36db073784f0218511ca5f2006bd99b9
Yeon-seong <dd300@naver.com>
2023-11-25 04:31:55 +0900

    트위터 클론 : .env 환경변수를 사용한 쿠키 비밀키 관리

--- back/app.js
+++ back/app.js
@@ -17,6 +17,10 @@ const cookieParser = require('cookie-parser');
 // Passport 미들웨어 호출
 const passport = require('passport');
 
+// Dotenv 모듈 호출
+const dotenv = require('dotenv');
+
+
 // 분리한 router 불러오기
 const postRouter = require('./routers/post');
 const userRouter = require('./routers/user');
@@ -27,6 +31,9 @@ const db = require('./models');
 // 패스포트에서 불러와 패스포트 설정 연결하기
 const passportConfig = require('./passport');
 
+
+// .env 파일 안에 있는 정보 불러오기
+dotenv.config();
 
 
 // express 서버 : 반환된 값을 app에 넣는다.
@@ -52,11 +59,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 쿠키와 세션 미들웨어 연결
-app.use(cookieParser('nodebirdsecret'));
+app.use(cookieParser(process.env.COOKIE_SECRET)); // 환경변수 사용
 app.use(session({
   saveUninitialized: false,
   resave: false,
-  secret: 'nodebirdsecret',
+  secret: process.env.COOKIE_SECRET,              // 환경변수 사용
 }));
 app.use(passport.initialize());
 app.use(passport.session());
