f3e5d1c161b2c36135ba8a6a2520e1e591392c7b
Yeon-seong <dd300@naver.com>
2023-11-25 04:41:18 +0900

    트위터 클론 : .evn 환경변수를 사용한 DB 비밀번호 관리

--- back/config/config.js
+++ back/config/config.js
@@ -1,21 +1,27 @@
+// Dotenv 모듈 호출
+const dotenv = require('dotenv');
+
+// .env 파일 안에 있는 정보 불러오기
+dotenv.config();
+
 module.exports = {
   "development": {
     "username": "root",
-    "password": "0000",
+    "password": process.env.DB_PASSWORD,
     "database": "react-nodebird",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": process.env.DB_PASSWORD,
     "database": "react-nodebird",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": null,
+    "password": process.env.DB_PASSWORD,
     "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql",
