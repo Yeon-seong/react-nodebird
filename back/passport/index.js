2a5bfa5050d97d7d3d1aae38c32d27c575b9ba19
Yeon-seong <dd300@naver.com>
2023-11-25 02:54:22 +0900

    트위터 클론 : 패스포트 로그인 시 쿠키 저장 및 사용자 정보 복구

--- back/passport/index.js
+++ back/passport/index.js
@@ -8,14 +8,29 @@ const passport = require('passport');
 // Local 파일 불러오기
 const local = require('./local');
 
+// 사용자 모델 불러오기
+const { User } = require('../models');
+
 
 
 module.exports = () => {
-  passport.serializeUser(() => {
-    // 패스포트 설정1
+  // 패스포트 설정1
+  passport.serializeUser((user, done) => {
+    /* 쿠키랑 묶어줄 정보(user.id)만 저장 */
+    done(null, user.id);
   });
-  passport.deserializeUser(() => {
-    // 패스포트 설정2
+
+  // 패스포트 설정2
+  /* 복원하기 위해 id를 통해서 user.id 전달 */
+  passport.deserializeUser(async (id, done) => {
+    try {
+      /* id를 통해 나머지 사용자 정보 복구 */
+      const user = await User.findOne({ where: { id }});
+      done(null, user);
+    } catch (error) {
+      console.error(error);
+      done(error);
+    }
   });
   
   // 로컬 실행 : local.js의 module.exports 실행
