34e13a5510ff370fb81ce08d1e64fb9154564be6
Yeon-seong <dd300@naver.com>
2023-11-25 02:37:39 +0900

    트위터 클론 : 로그인 기능 구현을 위한 쿠키와 세션 미들웨어 연결

--- back/app.js
+++ back/app.js
@@ -8,6 +8,15 @@ const express = require('express');
 // CORS 미들웨어 호출
 const cors = require('cors');
 
+// Express-Session 모듈 호출
+const session = require('express-session');
+
+// Cookie-Parser 모듈 호출
+const cookieParser = require('cookie-parser');
+
+// Passport 미들웨어 호출
+const passport = require('passport');
+
 // 분리한 router 불러오기
 const postRouter = require('./routers/post');
 const userRouter = require('./routers/user');
@@ -41,6 +50,17 @@ app.use(cors({ origin: true }));
 app.use(express.json());
 /* 폼을 제출했을 때 URL encoded 방식으로 넘어온 프론트에서 보낸 데이터를 req.body 안에 넣어줌 */
 app.use(express.urlencoded({ extended: true }));
+
+// 쿠키와 세션 미들웨어 연결
+app.use(cookieParser('nodebirdsecret'));
+app.use(session({
+  saveUninitialized: false,
+  resave: false,
+  secret: 'nodebirdsecret',
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // 메인 페이지 가져오기
 app.get('/', (req, res) => {
