c8f8466117cb10609e7dbb1c60861cb5cb0ac85d
Yeon-seong <dd300@naver.com>
2023-12-01 23:48:51 +0900

    트위터 클론 : 게시글 작성하기 라우터 수정 게시글 작성 성공 시 실제 게시글 데이터를 프론트로 돌려주기

--- back/routes/post.js
+++ back/routes/post.js
@@ -5,15 +5,29 @@
 // Express 모듈 호출
 const express = require('express');
 
+// 게시글 모델 불러오기
+const { Post } = require('../models');
+
 // 라우팅 모듈 호출
 const router = express.Router();
 
 
 
 // 게시글 작성하기 라우터
-router.post('/', (req, res) => {    // POST /post
-  res.json({ id: 1, content: 'hello' });
+router.post('/', async (req, res, next) => {    // POST /post
+  try {
+    const post = await Post.create({
+      content: req.body.content,  // addPost saga의 content: data
+      UserId: req.user.id,        // passport.deserializeUser로 사용자 정보 전달
+    });
+    /* 게시글 작성 성공 시 프론트로 돌려주기 */
+    res.status(201).json(post);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
+
 
 // 게시글 삭제하기 라우터
 router.delete('/', (req, res) => {  // DELETE /post
