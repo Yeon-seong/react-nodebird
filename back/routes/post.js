3dcaf17a0d8eccb0afa8e77723b7100ab4134bf4
Yeon-seong <dd300@naver.com>
2023-12-05 03:25:40 +0900

    트위터 클론 : 게시글의 이미지, 답글 작성자 정보를 추가해 실제 게시글 트윗 성공

--- back/routes/post.js
+++ back/routes/post.js
@@ -5,8 +5,8 @@
 // Express 모듈 호출
 const express = require('express');
 
-// 게시글, 답글 모델 불러오기
-const { Post, Comment } = require('../models');
+// 게시글, 이미지, 답글, 사용자 모델 불러오기
+const { Post, Image, Comment, User } = require('../models');
 
 // 로그인 유무를 검사하는 커스텀 미들웨어 불러오기
 const { isLoggedIn } = require('./middlewares');
@@ -19,12 +19,29 @@ const router = express.Router();
 // 게시글 작성하기 라우터
 router.post('/', isLoggedIn, async (req, res, next) => {  // POST /post
   try {
+    // 게시글 기본 정보
     const post = await Post.create({
       content: req.body.content,  // addPost saga의 content: data
       UserId: req.user.id,        // passport.deserializeUser로 사용자 정보 전달
     });
-    /* 게시글 작성 성공 시 프론트로 돌려주기 */
-    res.status(201).json(post);
+    // 게시글 모든 정보
+    const fullPost = await Post.findOne({
+      /* 방금 생성한 게시글 가져오기 */
+      where: { id: post.id },
+      // 모델 가져오기
+      include: [{
+        /* ---------- 게시글 이미지 ---------- */
+        model: Image,
+      }, {
+        /* ---------- 게시글 답글 ---------- */
+        model: Comment,
+      }, {
+        /* ---------- 게시글 작성자 ---------- */
+        model: User,
+      }]
+    });
+    /* 게시글 작성 성공 시 모든 정보를 완성해서 프론트로 돌려주기 */
+    res.status(201).json(fullPost);
     
   } catch (error) {
     console.error(error);
