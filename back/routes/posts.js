cd7d0c67c9dfe0b670066baeec4d897d6ca8499c
Yeon-seong <dd300@naver.com>
2023-12-10 01:15:40 +0900

    트위터 클론 : 최신 게시글부터 내림차순으로 게시글 10개 가져오기 시퀄라이즈에서 제공하는 limit, order 기능을 사용한 페이지네이션

--- back/routes/posts.js
+++ back/routes/posts.js
@@ -5,16 +5,30 @@
 // Express 모듈 호출
 const express = require('express');
 
-// 게시글, 사용자, 이미지 모델 불러오기
-const { Post, User, Image } = require('../models');
+// 게시글, 사용자, 이미지, 답글 모델 불러오기
+const { Post, User, Image, Comment } = require('../models');
 
 // 라우팅 모듈 호출
 const router = express.Router();
 
 
 
-// 게시글을 여러 개 가져오는 라우터
-router.get('/', async (req, res, next) => {});
+// 여러 개의 게시글을 가져오는 라우터
+router.get('/', async (req, res, next) => { // GET /posts
+  try {
+    /* Post.findAll : 지금까지 작성한 모든 게시글 가져오기 */
+    const posts = await Post.findAll({
+      /* 조건1 : 게시글 10개만 가져오기 */
+      limit: 10,
+      /* 조건2 : 최신 게시글부터 가져오기(내림차순) */
+      order: [['createdAt', 'DESC']],
+    });
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 
 
