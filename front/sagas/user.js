743a36a8ffbcfd33a766cdf6f0ce5c1cf99b34ad
Yeon-seong <dd300@naver.com>
2023-12-13 21:27:35 +0900

    트위터 클론 : 닉네임 변경 API 요청 및 사가 제작

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -11,12 +11,13 @@ import {
 import axios from 'axios';
 
 // 사용자 로그인, 로그아웃, 회원가입,
-// 사용자 정보 불러오기, 팔로우, 언팔로우 액션 불러오기
+// 사용자 정보 불러오기, 닉네임 변경, 팔로우, 언팔로우 액션 불러오기
 import {
   LOG_IN_REQUEST, LOG_IN_SUCCESS, LOG_IN_FAILURE,
   LOG_OUT_REQUEST, LOG_OUT_SUCCESS, LOG_OUT_FAILURE,
   SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE,
   LOAD_MY_INFO_REQUEST, LOAD_MY_INFO_SUCCESS, LOAD_MY_INFO_FAILURE,
+  CHANGE_NICKNAME_REQUEST, CHANGE_NICKNAME_SUCCESS, CHANGE_NICKNAME_FAILURE,
   FOLLOW_REQUEST, FOLLOW_SUCCESS, FOLLOW_FAILURE,
   UNFOLLOW_REQUEST, UNFOLLOW_SUCCESS, UNFOLLOW_FAILURE
 } from '../reducers/user';
@@ -123,6 +124,31 @@ function* loadMyInfo(action) {
 }
 
 
+// changeNickname 실행 시 서버에 changeNicknameAPI 요청
+function changeNicknameAPI(data) {
+  return axios.patch('/user/:nickname', {nickname: data});
+}
+// LOG_IN_REQUEST 액션이 실행되면 changeNickname 함수 실행
+function* changeNickname(action) {
+  /* ---------- 요청 성공 시 CHANGE_NICKNAME_SUCCESS 액션 디스패치 ---------- */
+  try {
+    const result = yield call(changeNicknameAPI, action.data);
+    yield put({
+      type: CHANGE_NICKNAME_SUCCESS,
+      data: result.data,         // 성공 결과 : 서버로부터 사용자 정보를 받아옴
+    });
+
+  /* ---------- 요청 실패 시 CHANGE_NICKNAME_FAILURE 액션 디스패치 ---------- */
+  } catch (err) {
+    console.error(err);
+    yield put({
+      type: CHANGE_NICKNAME_FAILURE,
+      error: err.response.data, // 실패 결과
+    });
+  }
+}
+
+
 // follow 실행 시 서버에 followAPI 요청
 function followAPI(data) {
   return axios.post('/api/follow', data);
@@ -200,6 +226,12 @@ function* watchloadMyInfo() {
 }
 
 
+// 닉네임 변경 액션
+function* watchChangeNickname() {
+  yield takeLatest(CHANGE_NICKNAME_REQUEST, changeNickname);
+}
+
+
 // 팔로우 액션
 function* watchFollow() {
   yield takeLatest(FOLLOW_REQUEST, follow);
@@ -221,6 +253,7 @@ export default function* userSaga() {
     fork(watchLogOut),
     fork(watchSignUp),
     fork(watchloadMyInfo),
+    fork(watchChangeNickname),
     fork(watchFollow),
     fork(watchUnFollow),
   ]);
