bfe80c5cc78ebec81d133e18663a0f0eb2a8eeec
Yeon-seong <dd300@naver.com>
2023-12-13 21:33:35 +0900

    트위터 클론 : 닉네임 수정 폼에서 닉네임을 변경할 수 있도록 액션객체 디스패치

--- front/components/NicknameEditForm.js
+++ front/components/NicknameEditForm.js
@@ -3,15 +3,35 @@
 
 
 // React 라이브러리 훅 불러오기
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
+
+// Redux 라이브러리 불러오기
+import { useDispatch, useSelector } from 'react-redux';
 
 // 외부 컴포넌트 불러오기
 import { Form, Input } from 'antd';
+
+// 내부 컴포넌트 불러오기
+import useInput from '../hooks/useInput';
+
+// 닉네임 변경 요청 액션 생성함수 불러오기
+import { CHANGE_NICKNAME_REQUEST } from '../reducers/user';
 
 
 
 // 닉네임 에디터 폼 컴포넌트(사용자 정의 태그)
 const NicknameEditForm = () => {
+  const { me } = useSelector((state) => state.user);
+  const [nickname, onChangeNickname] = useInput(me?.nickname || '');
+  const dispatch = useDispatch();
+
+  /* 닉네임 변경 폼 제출 시 닉네임 변경 요청 액션 객체 디스패치 */
+  const onSubmit = useCallback(() => {
+    dispatch({
+      type: CHANGE_NICKNAME_REQUEST,
+      data: nickname,
+    }); 
+  }, [nickname]);
 
   /* 닉네임 에디터 폼 컴포넌트 스타일 */
   const style = useMemo(() => ({
@@ -21,8 +41,13 @@ const NicknameEditForm = () => {
   }), []);
 
   return (
-    <Form style={style}>
+    <Form
+      style={style}
+      onFinish={onSubmit}
+    >
       <Input.Search
+        value={nickname}
+        onChange={onChangeNickname}
         addonBefore="닉네임"
         enterButton="수정"
         id="닉네임 수정 폼"
