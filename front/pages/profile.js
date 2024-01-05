ad366b877d863d03b05b1b748f5aa0cf8d35bf6d
Yeon-seong <dd300@naver.com>
2024-01-05 16:34:40 +0900

    트위터 클론 : getStaticProps를 사용한 프로필 페이지 서버사이드 렌더링

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -8,6 +8,12 @@ import React, { useEffect } from 'react';
 // Redux 라이브러리 Hook 불러오기
 import { useDispatch, useSelector } from 'react-redux';
 
+// Axios 라이브러리 불러오기
+import axios from 'axios';
+
+// END 액션 불러오기
+import { END } from 'redux-saga';
+
 // 외부 컴포넌트 불러오기
 import Head from 'next/head';
 import Router from 'next/router';
@@ -16,6 +22,9 @@ import Router from 'next/router';
 import AppLayout from '../components/AppLayout';
 import NicknameEditForm from '../components/NicknameEditForm';
 import FollowList from '../components/FollowList';
+
+// wrapper 불러오기
+import wrapper from '../store/configureStore';
 
 // 사용자 액션 불러오기
 import {
@@ -26,6 +35,9 @@ import {
   /* ---------- 팔로잉 불러오기 요청 액션 ---------- */
   LOAD_FOLLOWINGS_REQUEST,
 
+  /* ---------- 나의 사용자 정보 불러오기 요청 액션 ---------- */
+  LOAD_MY_INFO_REQUEST,
+  
 } from '../reducers/user';
 
 
@@ -77,20 +89,47 @@ const Profile = () => {
         <NicknameEditForm />
         
         {/* ---------- 팔로잉 목록 ---------- */}
-        <FollowList
-          header="팔로잉"
-          data={me.Followings}
-        />
+        <FollowList header="팔로잉" data={me.Followings} />
 
         {/* ---------- 팔로워 목록 ---------- */}
-        <FollowList
-          header="팔로워"
-          data={me.Followers}
-        />
+        <FollowList header="팔로워" data={me.Followers} />
       </AppLayout>
     </>
   );
 };
+
+
+
+// 서버사이드 렌더링(SSR) : getServerSideProps 사용
+/* 프로필 컴포넌트보다 먼저 실행, 매개변수 context 안에 store가 들어있다. */
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+
+  console.log('getServerSideProps start');
+  console.log(context.req.headers);
+
+  /* 변수 cookie에 모든 cookie 정보 저장 */
+  const cookie = context.req ? context.req.headers.cookie : '';
+
+  /* 쿠키를 안 써서 요청 보낼 때는 서버에서 공유하고 있는 쿠키를 제거하기 */
+  axios.defaults.headers.Cookie = '';
+
+  /* 서버일 때, 그리고 쿠키가 있을 때만 서버로 쿠키 전달하기 */
+  if (context.req && cookie) {
+    // 실제로 쿠키를 써서 요청을 보낼 때만 잠깐 쿠키를 넣어 놓는다.
+    axios.defaults.headers.Cookie = cookie;
+  }
+
+
+  /* 처음에 화면을 로딩하면 나의 사용자 정보 불러오기 요청 액션 객체 디스패치 */
+  context.store.dispatch({
+    type: LOAD_MY_INFO_REQUEST,
+  });
+
+  /* 나의 사용자 정보 요청(REQUEST)이 성공(SUCCESS)으로 바뀔 때까지 기다리기 */
+  context.store.dispatch(END);
+  console.log('getServerSideProps end');
+  await context.store.sagaTask.toPromise();
+});
 
 
 
