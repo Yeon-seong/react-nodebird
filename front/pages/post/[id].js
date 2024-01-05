636070e955fcef9c0d10d90418bbeb9d9751e1fb
Yeon-seong <dd300@naver.com>
2024-01-05 20:02:54 +0900

    트위터 클론 : getServerSideProps를 사용한 게시글 주소 페이지 서버사이드 렌더링

--- front/pages/post/[id].js
+++ front/pages/post/[id].js
@@ -5,8 +5,30 @@
 // React 라이브러리 불러오기
 import React from 'react';
 
+// Redux 라이브러리 Hook 불러오기
+import { useSelector } from 'react-redux';
+
 // 외부 컴포넌트 불러오기
 import { useRouter } from 'next/router';
+
+// Axios 라이브러리 불러오기
+import axios from 'axios';
+
+// END 액션 불러오기
+import { END } from 'redux-saga';
+
+// 내부 컴포넌트 불러오기
+import AppLayout from '../../components/AppLayout';
+import PostCard from '../../components/PostCard';
+
+// wrapper 불러오기
+import wrapper from '../../store/configureStore';
+
+// 나의 사용자 정보 불러오기 요청 액션 불러오기
+import { LOAD_MY_INFO_REQUEST } from '../../reducers/user';
+
+// 단일 게시글 불러오기 요청 액션 불러오기
+import { LOAD_POST_REQUEST } from '../../reducers/post';
 
 
 
@@ -18,13 +40,55 @@ const Post = () => {
 
   /* 구조분해로 아이디 받아오기 */
   const { id } = router.query;
+  
+  /* 중앙 데이터 저장소에서 상태 값 가져오기 */
+  const { singlePost } = useSelector((state) => state.post)
 
 
 
   return (
-    <div>{id}번 게시글</div>
+    <AppLayout>
+      <PostCard post={singlePost} />
+    </AppLayout>
   );
 };
+
+
+
+// 서버사이드 렌더링(SSR) : getServerSideProps 사용
+/* 게시글 컴포넌트보다 먼저 실행, 매개변수 context 안에 store가 들어있다. */
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+
+  /* 변수 cookie에 모든 cookie 정보 저장 */
+  const cookie = context.req ? context.req.headers.cookie : '';
+
+  console.log(context);
+
+  /* 쿠키를 안 써서 요청 보낼 때는 서버에서 공유하고 있는 쿠키를 제거하기 */
+  axios.defaults.headers.Cookie = '';
+
+  /* 서버일 때, 그리고 쿠키가 있을 때만 서버로 쿠키 전달하기 */
+  if (context.req && cookie) {
+    // 실제로 쿠키를 써서 요청을 보낼 때만 잠깐 쿠키를 넣어 놓는다.
+    axios.defaults.headers.Cookie = cookie;
+  }
+
+
+  /* 처음에 화면을 로딩하면 나의 사용자 정보 불러오기 요청 액션 객체 디스패치 */
+  context.store.dispatch({
+    type: LOAD_MY_INFO_REQUEST,
+  });
+
+  /* 처음에 화면을 로딩하면 단일 게시글 불러오기 요청 액션 객체 디스패치 */
+  context.store.dispatch({
+    type: LOAD_POST_REQUEST,
+    data: context.params.id, // 또는 context.query.id로 useRouter에 접근 가능
+  });
+
+  /* 나의 사용자 정보, 단일 게시글 불러오기 요청(REQUEST)이 성공(SUCCESS)으로 바뀔 때까지 기다리기 */
+  context.store.dispatch(END);
+  await context.store.sagaTask.toPromise();
+});
 
 
 
