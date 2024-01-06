d52947ee1e0cec864b5def9e96735270035fe071
Yeon-seong <dd300@naver.com>
2024-01-06 16:38:35 +0900

    트위터 클론 : ServerStyleSheet와 getInitialProps를 사용한 CSS 서버사이드 렌더링 스타일 컴포넌트 ServerStyleSheet와 next.js getInitialProps를 사용

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -2,12 +2,50 @@
 
 
 
+// React 라이브러리 불러오기
+import React from 'react';
+
 // 외부 컴포넌트 불러오기
 import Document, { Html, Main, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 
-// _document 파일 기본 구조
+
+// 서버사이드 렌더링(SSR) : getInitialProps 사용
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    /* ---------- React 렌더링 로직을 동기적으로 실행 ---------- */
+    try {
+      ctx.renderPage = () =>originalRenderPage({
+        /* enhanceApp으로 원래 app기능에 document 기능에다가
+           stylesheet가 styled-component들을 서버사이드 렌더링할 수 있게 해준다. */
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App { ...props } />)
+      });
+      // 부모 'getInitialProps'를 실행(이제 사용자 정의 'renderPage'가 포함된다.)
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    /* ---------- 에러 캐치 ---------- */
+    } catch (error) {
+      console.log(error);
+    /* ---------- try 블록이 종료되면 무조건 실행 ---------- */
+    } finally {
+      sheet.seal();
+    }
+  }
+
+
+
   render() {
     <Html lang="ko">
       <Head />
