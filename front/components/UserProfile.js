208a41ab87a5e49795cbfadbcf78af57bb1e224c
Yeon-seong <dd300@naver.com>
2024-01-11 18:07:41 +0900

    트위터 클론 : 나의 프로필 카드의 트윗, 팔로잉, 팔로워, 아바타 링크 코드 수정

--- front/components/UserProfile.js
+++ front/components/UserProfile.js
@@ -25,6 +25,7 @@ const UserProfile = () => {
 
   /* 중앙 데이터 저장소에서 상태 값 가져오기 */
   const { me, logOutLoading } = useSelector((state) => state.user);
+  console.log(me);
 
 
   // 더미데이터 로그아웃 콜백 함수
@@ -40,34 +41,46 @@ const UserProfile = () => {
         // 트윗을 누르면 내가 쓴 게시글 페이지로 이동하기
         <div key="twit">
           <Link href={`/user/${me.id}`}>
-            <a>트윗<br />{me.Posts.length}</a>
+            <a>
+              트윗
+              <br />
+              {me.Posts.length}
+            </a>
           </Link>
         </div>,
 
         // 팔로잉을 누르면 프로필 페이지로 이동하기
         <div key="following">
           <Link href="/profile">
-            <a>팔로잉<br />{me.Followings.length}</a>
+            <a>
+              팔로잉
+              <br />
+              {me.Followings.length}
+            </a>
           </Link>
         </div>,
 
         // 팔로워를 누르면 프로필 페이지로 이동하기
         <div key="follower">
           <Link href="/profile">
-            팔로워<br />{me.Followers.length}
+            <a>
+              팔로워
+              <br />
+              {me.Followers.length}
+            </a>
           </Link>
         </div>,
       ]}
     >
 
       <Card.Meta
-        avatar={
-          // 나의 사용자 프로필 카드에서 아바타를 누르면 내가 쓴 게시글 페이지로 이동하기
+        avatar={(
+          /* 나의 사용자 프로필 카드에서 아바타를 누르면 내가 쓴 게시글 페이지로 이동하기
+             닉네임의 첫 번째 글자를 아바타 아이콘으로 표시 */
           <Link href={`/user/${me.id}`}>
-            {/* 닉네임의 첫 번째 글자를 아바타 아이콘으로 표시 */}
             <a><Avatar>{me.nickname[0]}</Avatar></a>
           </Link>
-        }
+        )}
         title={me.nickname}
       />
       {/* ---------- 로딩 중 버튼 ---------- */}
