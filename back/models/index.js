af53ac3f601c38c26b0a47da637609154be56525
Yeon-seong <dd300@naver.com>
2024-01-14 18:49:13 +0900

    트위터 클론 : Model 클래스로 확장한 모델 불러오기 및 모델 설정 실행하기

--- back/models/index.js
+++ back/models/index.js
@@ -5,6 +5,15 @@
 // SQL(시퀄라이즈) 불러오기
 const Sequelize = require('sequelize');
 
+// 모델 불러오기
+const comment = require('./comment');
+const hashtag = require('./hashtag');
+const image = require('./image');
+const post = require('./post');
+const user = require('./user');
+
+
+
 // 개발 시 환경 변수 기본 값 : development
 const env = process.env.NODE_ENV || 'development';
 
@@ -15,26 +24,36 @@ const config = require('../config/config')[env];
 const db = {};
 
 
+
 // 시퀄라이즈 객체에 연결 정보가 담김
 const sequelize = new Sequelize(
   config.database, config.username, config.password, config
 );
 
 
-// 모델 불러오기
-db.User = require('./user')(sequelize, Sequelize);
-db.Post = require('./post')(sequelize, Sequelize);
-db.Comment = require('./comment')(sequelize, Sequelize);
-db.Hashtag = require('./hashtag')(sequelize, Sequelize);
-db.Image = require('./image')(sequelize, Sequelize);
+
+// 클래스 불러오기
+db.Comment = comment;
+db.Hashtag = hashtag;
+db.Image = image;
+db.Post = post;
+db.User = user;
 
 
-// 모델 관계 설정 실행
+// 모델 설정 실행(init)
+Object.keys(db).forEach(modelName => {
+  db[modelName].init(sequelize);
+});
+
+
+// 모델 관계 설정 실행(associate)
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
+
+
 
 // db에 시퀄라이즈 넣기
 db.sequelize = sequelize;
