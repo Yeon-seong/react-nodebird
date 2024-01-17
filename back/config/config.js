a6c9e9fd93892673fbdf43598e43e5f0c10750c1
Yeon-seong <dd300@naver.com>
2024-01-17 17:52:37 +0900

    트위터 클론 : config database 수정

--- back/config/config.js
+++ back/config/config.js
@@ -6,26 +6,26 @@ dotenv.config();
 
 // module.exports 객체
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": process.env.DB_PASSWORD,
-    "database": "react-nodebird",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  development: {
+    username: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'react-nodebird',
+    host: '127.0.0.1',
+    dialect: 'mysql',
   },
-  "test": {
-    "username": "root",
-    "password": process.env.DB_PASSWORD,
-    "database": "react-nodebird",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'react-nodebird',
+    host: '127.0.0.1',
+    dialect: 'mysql',
   },
-  "production": {
-    "username": "root",
-    "password": process.env.DB_PASSWORD,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  }
-}
+  production: {
+    username: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'react-nodebird',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false
+  },
+};
