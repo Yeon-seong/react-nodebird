6c5fef9d8d4a9e38ab88419a29351044fb7c29b8
Yeon-seong <dd300@naver.com>
2024-02-03 14:51:21 +0900

    트위터 클론 : MySQL 사용자 모델 문자셋을 utf8mb4로 변경하기

--- back/models/user.js
+++ back/models/user.js
@@ -43,7 +43,7 @@ module.exports = class User extends Model {
       /* 사용자 모델 세팅 */
       modelName: 'User',          // 모델 이름 : User
       tableName: 'users',         // MySQL 테이블 이름 : users(소문자 복수)
-      charset: 'utf8',            // MySQL에서 한글 사용 가능
+      charset: 'utf8mb4',         // MySQL에서 한글, 이모티콘 사용 가능
       collate: 'utf8_general_ci', // 한글 저장
       sequelize,                  // 연결 객체를 클래스로 보내주기 위해 넣어준다.
 
