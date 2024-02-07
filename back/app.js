6dcf39ae755ac7ae914c4b5f6d4b1ceddc0c2486
Yeon-seong <dd300@naver.com>
2024-02-07 20:36:54 +0900

    트위터 클론 : 데이터베이스 시퀄라이즈 연결에서 force를 false로 설정하기

--- back/app.js
+++ back/app.js
@@ -55,8 +55,8 @@ dotenv.config();
 const app = express();
 
 // 서버 실행 시 데이터베이스 시퀄라이즈 연결
-/* force: true는 테이블이 존재하지 않을 경우 테이블을 생성, 존재할 경우 테이블을 삭제하기 */
-db.sequelize.sync({ force: true })
+/* DB에 테이블이 없다면 force를 true로, DB에 테이블이 있다면 force를 false로 설정 */
+db.sequelize.sync({ force: false })
   .then(() => {
     console.log('db 연결 성공');
   })
